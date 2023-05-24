f5700f03a7f22ab8cbb32cf5f76efd9dde8d72a0
keemsebin <sebin0580@gmail.com>
2023-05-24 15:18:18 +0900

    Feat : Splash 화면 구현

--- app/src/main/java/com/example/a23_mju_mc_project/MainActivity.kt
+++ app/src/main/java/com/example/a23_mju_mc_project/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.a23_mju_mc_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a23_mju_mc_project.databinding.ActivityMainBinding
+import com.example.a23_mju_mc_project.databinding.ActivitySplashBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
