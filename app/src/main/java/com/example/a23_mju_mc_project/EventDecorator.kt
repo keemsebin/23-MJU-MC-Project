69cd79429a05defd10b1e37f156a63d8f194bf53
nwn <feyfey927@gmail.com>
2023-06-08 13:35:50 +0900

    Feed 작성 시 달력 상 해당 날짜 아래 점으로 표시 기능 추가 MainActivity에서 디바이스 뒤로가기 두번 누를 시 앱이 종료되는게 아니라 InputFragment로 돌아가는 오류 수정

--- app/src/main/java/com/example/a23_mju_mc_project/EventDecorator.kt
+++ app/src/main/java/com/example/a23_mju_mc_project/EventDecorator.kt
@@ -7,8 +7,8 @@ import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.spans.DotSpan
 
 // 이벤트가 발샐한 날짜에 점을 찍어주는 클래스
-class EventDecorator(dates: Collection<CalendarDay>) : DayViewDecorator {
-   // dates를 순서를 지정하지 않고 저장
+class EventDecorator(dates: MutableSet<CalendarDay>) : DayViewDecorator {
+    // dates를 순서를 지정하지 않고 저장
     var dates : HashSet<CalendarDay> = HashSet(dates)
     // 주어진 day가 장식되어야 하는 날짜인지 결정. dates에 day가 포함 되는지에 따라 true/false반환
     override fun shouldDecorate(day: CalendarDay?): Boolean {
